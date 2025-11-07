5a0c3e6b728e69b67115805f56623a836c1af0b4
Nikita <nikitadev-work@mail.ru>
2025-11-07 23:07:08 +0300

    feat: create base of purchase service

---
+++ services/purchase/go.mod
@@ -0,0 +1,7 @@
+module purchase
+
+go 1.23.0
+
+toolchain go1.24.10
+
+require github.com/nikitadev-work/SyncBuyProject/common/kit v0.0.0-20251107193235-2402d2212a88
---
+++ services/purchase/internal/adapters/http/server.go
@@ -0,0 +1,14 @@
+package httpserver
+
+import (
+	"net/http"
+
+	"github.com/nikitadev-work/SyncBuyProject/common/kit/logger"
+)
+
+func New(logger logger.LoggerInterface, mux *http.ServeMux, port string) *http.Server {
+	return &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+}
